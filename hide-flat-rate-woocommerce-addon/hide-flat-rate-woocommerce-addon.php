2b5fb27c121ab7436bce1d4bbc300b2fcb467e29
HassaElgallouchi <helgallouchi@gmail.com>
2021-03-19 06:55:15 +0100

    changer le repo dans local

---
+++ hide-flat-rate-woocommerce-addon/hide-flat-rate-woocommerce-addon.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Hide flat rate shipping
+Plugin URI: https://github.com/HassanElgallouchi/wordpress-plugin-woocommerce
+Description: hide flat rate when free shipping woocommerce.
+Author: Hassan El gallouchi
+Version: 1.0.0
+Author URI: https://github.com/HassanElgallouchi
+*/
+
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ * 
+ */
+function my_hide_shipping_when_free_is_available( $rates ) {
+    $free = array();
+    foreach ( $rates as $rate_id => $rate ) {
+        if ( 'free_shipping' === $rate->method_id ) {
+            $free[ $rate_id ] = $rate;
+            break;
+        }
+    }
+    return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
